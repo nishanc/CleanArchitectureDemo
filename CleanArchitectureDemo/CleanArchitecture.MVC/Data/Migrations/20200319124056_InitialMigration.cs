bfa1c12153a255d99ce6a11eda9dddc06cdbc499
Nishan Chathuranga <inbox.nishan@gmail.com>
2020-03-19 19:49:43 +0530

    MVC project and identity migration

---
+++ CleanArchitectureDemo/CleanArchitecture.MVC/Data/Migrations/20200319124056_InitialMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.MVC.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CleanArchitectureDemo/CleanArchitecture.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitecture.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
