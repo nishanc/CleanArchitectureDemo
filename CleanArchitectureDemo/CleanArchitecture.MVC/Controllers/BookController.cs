cce4b86ccc6ab10f5ad06e73f05d1fe674c4b0fd
Nishan Chathuranga <inbox.nishan@gmail.com>
2020-03-20 22:00:07 +0530

    get list of books to presentation layer

---
+++ CleanArchitectureDemo/CleanArchitecture.MVC/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.MVC.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            BookViewModel model = _bookService.GetBooks();
+            return View(model);
+        }
+    }
+}
